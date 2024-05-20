56f1678f4b28ab3cbf1bc3ae88204acde7f239ed
Azdin Mediri <a.elmediri@gmail.com>
2024-05-20 21:46:44 +0200

    Start Next App

--- helloworld.py
+++
@@ -1,9 +0,0 @@
-class Hello():
-    def __init__(self, name : str):
-        self.name = "Hello, World!"
-
-    def say_hello(self):
-        print(f"Hello {self.name}, i'm a class")
-
-hello = Hello("World")
-hello.say_hello()
