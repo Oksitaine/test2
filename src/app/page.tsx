ff93064d5b424c7f4c3b0865bded9a42b720e5aa
Hamza-Zaaboul <hamzzaaboul@gmail.com>
2024-05-20 21:51:52 +0200

    Ajout Btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,6 +39,16 @@ export default function Home() {
         />
       </div>
 
+    
+  
+      <div> 
+        <button className="bg-white rounded-xl py-3 px-5 text-black"> 
+          APPUUYER ICI CI TU EST UN PD
+        </button>
+      </div>
+
+
+
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
