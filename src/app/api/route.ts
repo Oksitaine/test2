e46db65a5757698c39b8823ab71a515ea5be1014
Azdin Mediri <a.elmediri@gmail.com>
2024-05-20 22:08:42 +0200

    ADD POST REQUEST

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -10,3 +10,13 @@ export async function GET(req: Request){
         status: 200
     })
 }
+
+export async function POST(req: Request){
+    const res = await req.json();
+
+    return NextResponse.json({
+        "message": "Les loutres sont mignonnes mais ca ne vaut pas des robots de 100 metres de haut comme WGlint !!!",
+    }, {
+        status: 200
+    })
+}   
